6788dc916cee52238aa6ace3c614c05d75a70bb8
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-23 20:47:17 +0530

    refactoring routes

---
+++ src/components/routes.jsx
@@ -0,0 +1,16 @@
+import App from "../App";
+import Home from "./Home";
+import Shop from "./Shop";
+import Cart from "./Cart";
+
+const routes = [{
+    path: "/",
+    element: <App />,
+    children: [
+        { path: "home", element: <Home />},
+        { path: "shop", element: <Shop />},
+        { path: "cart", element: <Cart />},
+    ]
+}]
+
+export default routes
