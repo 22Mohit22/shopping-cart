763db0a90bccbe908d5f99d4d15de59fc30158e4
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-23 20:49:06 +0530

    navbar updated and styles applied to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,23 @@
+import "../styles/navstyles.css"
+
 import React from "react"
 import { Link } from "react-router-dom"
 
 export default function NavBar() {
     return (
-        <div>
-            <header>
-                Khareedo.com
-            </header>
+        <div className="nav-container">
             <nav>
-                <ul>
+                <header>
+                    <Link to="/"><h1>Khareedo.com</h1></Link>
+                </header>
+                <ul className="nav-links">
                     <li><Link to="home">Home</Link></li>
                     <li><Link to="shop">Shop</Link></li>
                     <li><Link to="cart">Cart</Link></li>
+                </ul>
+                <ul className="acc-link">
+                    <li>Mohit Pandey</li>
+                    <li>Logout</li>
                 </ul>
             </nav>
         </div>
