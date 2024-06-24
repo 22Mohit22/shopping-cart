cc10e73a647b25d025d24430c25e14adb0456878
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-24 10:51:37 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router";
+import "../styles/homestyles.css"
+import Arrow from "./Arrow";
+
 export default function Home() {
+
+    const navigate = useNavigate();
+
+    function loadShop() {
+        navigate("/shop");
+    }
+
     return (
-        <h1>Home page</h1>
+        <div className="home">
+            <p className="headline">
+                Discover Your Perfect Style - Shop Trendy, Affordable Fashion at Your Fingertips!
+            </p>
+
+            <button className="shop-btn" onClick={loadShop}>
+                <span className="shopBtn-text">Shop Now</span> <Arrow />
+            </button>
+        </div>
     )
 }
