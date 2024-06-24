1d87b8cc243e6b65845f4bd3d0a1f13cf66748d4
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-24 10:51:22 +0530

    Create Arrow.jsx

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,11 @@
+import "../styles/arrowStyles.css"
+
+export default function Arrow() {
+    return (
+        <span className="arrow">
+            <div className="arrow-tail"></div>
+            <div className="arrow-top"></div>
+            <div className="arrow-bottom"></div>
+        </span>
+    )
+}
