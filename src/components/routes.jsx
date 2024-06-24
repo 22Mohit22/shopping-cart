db79d18f70cd30ed336eaaa570ca9a246f576e1f
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-24 10:52:02 +0530

    fixed routes

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -7,7 +7,7 @@ const routes = [{
     path: "/",
     element: <App />,
     children: [
-        { path: "home", element: <Home />},
+        { path: "/", element: <Home />},
         { path: "shop", element: <Shop />},
         { path: "cart", element: <Cart />},
     ]
