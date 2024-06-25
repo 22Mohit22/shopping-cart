f0bdb25c9c31a45615e5d5c61b65a23e9fa0ee62
Mohit_kumar_pandey <93579686+22Mohit22@users.noreply.github.com>
2024-06-25 15:09:44 +0530

    added category functionality

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,35 +1,53 @@
-import Product from "./Product";
 import "../styles/shopstyles.css"
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Product from "./Product";
 
 export default function Shop() {
 
-    const [data, setData] = useState(null);
     const [categories, setCategories] = useState(null);
-    
+    const [products, setProducts] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState('');
+
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(data=> (console.log(data), setData(data)))
-    },[])
+        fetch(`https://fakestoreapi.com/products`)
+        .then(res => res.json())
+        .then(prod => setProducts(prod))
+    }, [])
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products/categories')
             .then(res => res.json())
-            .then(category => (console.log(category), setCategories(category)))
+            .then(category => setCategories(category))
     }, [])
+
+
+    
+    useEffect(() => {
+        if (selectedCategory != 'all') {
+            fetch(`https://fakestoreapi.com/products/category/${selectedCategory}`)
+            .then(res => res.json())
+            .then(prod => setProducts(prod))
+        } else {
+            fetch(`https://fakestoreapi.com/products`)
+            .then(res => res.json())
+            .then(prod => setProducts(prod))
+        }
+    }, [selectedCategory])
+
+    function categoryWise(category) {
+        setSelectedCategory(category);
+    }
 
     return (
         <div className="shop-area">
-            <select name="categories" id="categories">
-                <option value="all">all</option>
-                {categories? categories.map(cat => <option value={cat}>{cat}</option>) : <option value={'none'}>No option available</option>}
-            </select>
+            
+            <div className="categories">
+                <button onClick={() => setSelectedCategory('all')} className="category-btn">All</button>
+                {categories ? categories.map(cat => <button onClick={() => categoryWise(cat)} className="category-btn" key={cat}  >{cat.toUpperCase()}</button>) : <option value={'none'}>No Categories available</option>}
+            </div>
+            
             <div className="product-con">
-                {data ? data.map(product =>
-                        <Product key={product.id} name={product.title} img={product.image} price={product.price} ratings={product.rating.rate} procat={product.category} />
-                    ) : <h1>Loading Products...</h1>}
+                {products ? products.map(product => <Product key={product.id} img={product.image} name={product.title} price={product.price} ratings={product.rating.rate} procat={product.category} />) : <h2>Loading products...</h2>}
             </div>
         </div>
     )
